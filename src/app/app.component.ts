e281a25cd234e8603ff8f49a7320f3b0544f90fe
Nattalie <Lily@Nattalies-MacBook-Air.local>
2017-11-17 16:37:00 +0700

    Second one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.=']
+  styleUrls: ['./app.component.css'],
+
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'FixMe Abac';
+  report: Observable<any[]>;
+  constructor(private db: AngularFireDatabase) {
+    this.report = this.db.list('/report/open').valueChanges();
+    console.log(this.report);
+  }
+
+  ngInit(){
+    this.fetch();
+  }
+  fetch(){
+    
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { DetailComponent } from './detail/detail.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD9wBjZlpm2qOyUlnzh4Ax_8a22p2kTayo",
+  authDomain: "fixmeabac.firebaseapp.com",
+  databaseURL: "https://fixmeabac.firebaseio.com",
+  projectId: "fixmeabac",
+  storageBucket: "",
+  messagingSenderId: "483379790841"
+};
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [ AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
